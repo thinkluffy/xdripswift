efe39c3d4798a625c4647f82f0555e74f0630354
Yuanbin Cai <yuanbin_cai@thinkyeah.com>
2021-10-25 18:15:41 +0800

    developing
    Signed-off-by: Yuanbin Cai <yuanbin_cai@thinkyeah.com>

---
+++ xDripWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,27 @@
+//
+//  InterfaceController.swift
+//  xDripWatch WatchKit Extension
+//
+//  Created by Yuanbin Cai on 2021/10/25.
+//  Copyright © 2021 Johan Degraeve. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ xDripWatch WatchKit Extension/NotificationController.swift
@@ -0,0 +1,35 @@
+//
+//  NotificationController.swift
+//  xDripWatch WatchKit Extension
+//
+//  Created by Yuanbin Cai on 2021/10/25.
+//  Copyright © 2021 Johan Degraeve. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
