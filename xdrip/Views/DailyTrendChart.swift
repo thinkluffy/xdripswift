ca12ab930e30b71c4b7dcc636d433f37f77652e7
Yuanbin Cai <yuanbin_cai@thinkyeah.com>
2022-01-15 17:57:04 +0800

    developing
    Signed-off-by: Yuanbin Cai <yuanbin_cai@thinkyeah.com>

---
+++ xdrip/Views/DailyTrendChart.swift
@@ -0,0 +1,30 @@
+//
+//  DailyTrendChart.swift
+//  xdrip
+//
+//  Created by Yuanbin Cai on 2022/1/15.
+//  Copyright © 2022 zDrip. All rights reserved.
+//
+
+import UIKit
+
+class DailyTrendChart: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
